be2da9c24983770f3adfe4c3fe7f7ea8885a0839
Julian Ritou <julian.ritou@gmail.com>
2026-05-11 14:02:25 +0200

    Test de la route GET URI '/api/books'

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next(); 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.use((req, res,next) => {
-    res.send('Voilà la réponse du serveur Mon Vieux Grimoire !');
+app.get('/api/books', (req, res, next) => {
+  const books = [
+    {
+      _id: '0987654321',
+      userid: 'julian',
+      title: 'Livre1',
+      author: 'Julian Ritou',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      year: 2026,
+      genre: 'fiction',
+      ratings: [],
+      averageRating: 0,      
+    },
+    {
+      _id: '1234567890',
+      userid: 'julian',
+      title: 'Livre2',
+      author: 'Julian Ritou',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      year: 2025,
+      genre: 'fiction',
+      ratings: [],
+      averageRating: 0, 
+    },     
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;

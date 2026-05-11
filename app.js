96f0acf34305ef048d6728f08dcd8befbae3a006
Julian Ritou <julian.ritou@gmail.com>
2026-05-11 18:24:02 +0200

    connexion à la base de donnée MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 

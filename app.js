b7e2ac55cfc45af79deb1b1f3050a20b19e3e5d9
Julian Ritou <julian.ritou@gmail.com>
2026-05-11 13:15:21 +0200

    amélioration server.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next(); 
+});
+
+app.use((req, res,next) => {
     res.send('Voilà la réponse du serveur Mon Vieux Grimoire !');
 });
 

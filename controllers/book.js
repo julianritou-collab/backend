1d2630f64d519fe370568d045603a77504d17eba
Julian Ritou <julian.ritou@gmail.com>
2026-05-13 12:04:39 +0200

    svg test ok route DELETE '/api/books/:id'

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book');
+const fs = require('fs');
 
 exports.getAllBooks = (req, res, next) => {
     Book.find().then(
@@ -69,6 +70,22 @@ exports.modifyBook = (req, res, next) => {
 };
 
 exports.deleteBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id})
+    .then(book => {
+      if (book.userId != req.auth.userId) {
+          res.status(401).json({ message : 'Not authorized'});
+      } else {
+          const filename = book.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => { 
+          Book.deleteOne({ _id: req.params.id})
+              .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+              .catch(error => res.status(401).json({ error }));
+          });
+      }
+    })
+    .catch((error) => {
+        res.status(400).json({ error });
+    });
 };
 
 exports.rateBook = (req, res, next) => {

e3026b60eb6b8e793c44e52b3cb7489aa52aad55
Julian Ritou <julian.ritou@gmail.com>
2026-05-15 10:04:30 +0200

    gestion d'un sous schema ratingSchema sans _id dans bookSchema

--- models/Book.js
+++ models/Book.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const ratingSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  grade: { type: Number, required: true },
+}, { _id: false });
+
 const bookSchema = mongoose.Schema({
   userId: { type: String, required: true },
   title: { type: String, required: true },
@@ -7,12 +12,7 @@ const bookSchema = mongoose.Schema({
   imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
   genre: { type: String, required: true },
-  ratings: [
-    {
-      userId: { type: String, required: true },
-      grade: { type: Number, required: true },
-    },
-  ],
+  ratings: [ratingSchema],
   averageRating: { type: Number, required: true },
 });
 
